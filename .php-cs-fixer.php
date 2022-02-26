afc59ff323065a1cde2d6beb05af62c68f05598f
liber <liber@shawly.cn>
2022-02-26 19:39:29 +0800

    update cs-fixer configuration, upgrade cs-fixer version

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,49 +1,57 @@
 <?php
 
-$header = <<<'EOF'
-This file is part of Shoplinke.
-Developed By Middle Platform Team Of Starlinke
+declare(strict_types=1);
+/**
+ * This file is part of Little Bookkeeping Project.
+ * Developed By Shawly
+ *
+ * @link     https://www.shawly.cn
+ * $contact  liber@shawly.cn
+ */
 
-@link     https://www.starlinke.com
-@document https://starlink.feishu.cn/docs/doccn3jY4If0LrKByUrZvlusX0B
-$contact  dev@starlinke.com
+$header = <<<'EOF'
+This file is part of Little Bookkeeping Project.
+Developed By Shawly
+
+@link     https://www.shawly.cn
+$contact  liber@shawly.cn
 EOF;
 
-return PhpCsFixer\Config::create()
+return (new PhpCsFixer\Config())
     ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR2' => true,
-        '@Symfony' => true,
+        '@PSR2'               => true,
+        '@Symfony'            => true,
         '@DoctrineAnnotation' => true,
-        '@PhpCsFixer' => true,
-        'header_comment' => [
-            'commentType' => 'PHPDoc',
-            'header' => $header,
-            'separate' => 'none',
-            'location' => 'after_declare_strict',
+        '@PhpCsFixer'         => true,
+        'header_comment'      => [
+            'comment_type' => 'PHPDoc',
+            'header'       => $header,
+            'separate'     => 'none',
+            'location'     => 'after_declare_strict',
         ],
         'array_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'list_syntax' => [
-            'syntax' => 'short'
+            'syntax' => 'short',
         ],
         'concat_space' => [
-            'spacing' => 'one'
+            'spacing' => 'one',
         ],
         'trim_array_spaces' => true,
-        'braces' => [
-            'allow_single_line_closure' => true
+        'braces'            => [
+            'allow_single_line_closure' => true,
         ],
         'blank_line_after_opening_tag' => true,
-        'blank_line_before_statement' => [
+        'blank_line_before_statement'  => [
             'statements' => [
                 'declare', 'return',
             ],
         ],
         'general_phpdoc_annotation_remove' => [
             'annotations' => [
-                'author'
+                'author',
             ],
         ],
         'ordered_imports' => [
@@ -58,8 +66,8 @@ return PhpCsFixer\Config::create()
         ],
         'yoda_style' => [
             'always_move_variable' => false,
-            'equal' => false,
-            'identical' => false,
+            'equal'                => false,
+            'identical'            => false,
         ],
         'phpdoc_align' => [
             'align' => 'left',
@@ -73,23 +81,22 @@ return PhpCsFixer\Config::create()
         'binary_operator_spaces' => [
             'default' => 'align_single_space_minimal',
         ],
-        'class_attributes_separation' => true,
-        'combine_consecutive_unsets' => true,
-        'declare_strict_types' => true,
-        'linebreak_after_opening_tag' => true,
-        'lowercase_static_reference' => true,
-        'no_useless_else' => true,
-        'no_unused_imports' => true,
+        'class_attributes_separation'       => true,
+        'combine_consecutive_unsets'        => true,
+        'declare_strict_types'              => true,
+        'linebreak_after_opening_tag'       => true,
+        'lowercase_static_reference'        => true,
+        'no_useless_else'                   => true,
+        'no_unused_imports'                 => true,
         'not_operator_with_successor_space' => true,
-        'not_operator_with_space' => false,
-        'no_multiline_whitespace_before_semicolons' => true,
-        'no_short_echo_tag' => true,
-        'declare_equal_normalize' => ['space' => 'none'],
-        'ordered_class_elements' => true,
-        'php_unit_strict' => false,
-        'phpdoc_separation' => false,
-        'single_quote' => true,
-        'standardize_not_equals' => true,
+        'not_operator_with_space'           => false,
+        'echo_tag_syntax'                   => ['format' => 'long'],
+        'declare_equal_normalize'           => ['space' => 'none'],
+        'ordered_class_elements'            => true,
+        'php_unit_strict'                   => false,
+        'phpdoc_separation'                 => false,
+        'single_quote'                      => true,
+        'standardize_not_equals'            => true,
         'multiline_comment_opening_closing' => true,
     ])
     ->setFinder(
